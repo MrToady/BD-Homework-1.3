601383db407e4cc2ca7d73a4dc0bc89e50915a9c
Taras Moisiuk <Taras_Moisiuk@epam.com>
2018-01-19 13:02:05 +0300

    implement custom partitioner

--- src/main/java/HighBitPriceJob.java
+++ src/main/java/HighBitPriceJob.java
@@ -20,14 +20,14 @@ public class HighBitPriceJob {
         job.setReducerClass(HighBitPriceReducer.class);
         job.setCombinerClass(HighBitPriceCombiner.class);
         job.setSortComparatorClass(IntWritable.Comparator.class);
-//        job.setPartitionerClass(HighBitPricePartitioner.class);
+        job.setPartitionerClass(HighBitPricePartitioner.class);
 
         job.setMapOutputKeyClass(IntWritable.class);
         job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-//        job.setNumReduceTasks(Runtime.getRuntime().availableProcessors() - 1);
+        job.setNumReduceTasks(Runtime.getRuntime().availableProcessors() - 1);
 //        job.addCacheFile(URI.create("/tmp/taras/city.en.txt"));
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
--- src/main/java/HighBitPricePartitioner.java
+++ src/main/java/HighBitPricePartitioner.java
@@ -1,24 +1,10 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class HighBitPricePartitioner extends Partitioner<IntWritable, IntWritable>{
+public class HighBitPricePartitioner extends Partitioner<IntWritable, HighBitOSWritable>{
 
     @Override
-    public int getPartition(IntWritable intWritable, IntWritable intWritable2, int numPartitions) {
-
-
-
-
-
-
-
-
-
-        //TODO Сделать кастомный варйтабл который будет содержать юзер агент и оперейшн систем и по ней уже партишенер будет выбриарать куда засунуть.
-        //TODO 1. Из маппера выходит кастомный врайтбл с оп систем
-        //TODO 2. комбайнер не комбайнит????????
-        //TODO 3. Партишнер опрделеяет по ОС куда пойдет пара
-        //TODO
-        return 0;
+    public int getPartition(IntWritable key, HighBitOSWritable value, int numReduceTasks) {
+        return (value.getOperationSystem().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
